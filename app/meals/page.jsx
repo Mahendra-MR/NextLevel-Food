3fb2b671063d9874bd0449cde5065c51bd188344
Mahendra-MR <mrmahendra1206@gmail.com>
2025-04-25 21:28:36 +0530

    first commit

---
+++ app/meals/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MealsPage = () => {
+  return (
+    <div>mealsPage</div>
+  )
+}
+
+export default MealsPage
---
+++ app/meals/share/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShareMealPage = () => {
+  return (
+    <div>MealShare</div>
+  )
+}
+
+export default ShareMealPage
